0a220d09701cadf1487c0d6c49c3afc10c95a0e2
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-05-22 08:09:37 +0300

    Revert "Delete Test.py"
    This reverts commit 14ca95cd204ad80c20a893e503ce3a64a432b439.

---
+++ Test.py
@@ -0,0 +1,12 @@
+print('First RAG Project!')
+print('Testing VS Code!')
+
+S = 5
+x = int(input('Enter a number: '))
+
+if S > x:
+    print('S is greater than x')
+elif S < x:
+    print('S is less than x')
+else:
+    print('S is equal to x')
