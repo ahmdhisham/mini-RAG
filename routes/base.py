2d0b6324eb5991e59d4ff7d27db07efb9a51380d
ahmdhisham <ahmedhisham8546@gmail.com>
2025-06-03 15:10:08 +0300

    Added arguments to APIRouter

--- routes/base.py
+++ routes/base.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, APIRouter
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix='api/v1',
+    tags='api_v1',
+)
 
 @base_router.get("/")
 def welcome():
-    return {"message":"Hello World!"}
+    return {"message":"Hello All!"}
