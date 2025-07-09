8fb743c942ef6b75153fbe5f2cde44463235d17d
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-07-09 20:12:32 +0300

    .

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,6 +1,6 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
 import os
-from helpers.config import get_settings
+from src.helpers.config import get_settings, Settings
 
 base_router = APIRouter(
     prefix='/api/v1',
@@ -8,9 +8,7 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
-    
-    app_settings = get_settings()
+async def welcome(app_settings: Settings = Depends(get_settings)):
     
     app_name = app_settings.APP_NAME
     app_version = app_settings.APP_VERSION
