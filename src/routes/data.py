014996ba07e6e81d399125dc4ab30bc46fbe1d1c
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-07-09 20:12:52 +0300

    Update data router

---
+++ src/routes/data.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, APIRouter, Depends, UploadFile
+import os
+from src.helpers.config import get_settings, Settings
+from src.controllers.DataControllers import DataController
+
+data_router = APIRouter(
+    prefix = "/api/v1/data",
+    tags = ["api_v1", "data"]
+)
+
+@data_router.post("/upload/{project_id}")
+async def upload_file(project_id: str, file: UploadFile, 
+                    app_settings: Settings = Depends(get_settings)):
+    # validate the file type and size
+    is_valid = DataController().validate_uploaded_file(file = file)
+    
+    return is_valid
