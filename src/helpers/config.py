127a4087f93cd71da5f9df09cd098b37246c4dff
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-07-09 20:12:24 +0300

    .

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,9 +4,12 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
     
     class Config:
-        env_file = ".env"
+        env_file = "src/.env"
+        env_file_encoding = "utf-8"
         
 def get_settings():
     return Settings()
