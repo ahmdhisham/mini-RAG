6ecf98edbc2b10188a6e764a85a8ae73f588ad18
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-07-09 20:12:17 +0300

    Update DataController

---
+++ src/controllers/DataControllers.py
@@ -0,0 +1,20 @@
+from .BaseController import BaseController
+from fastapi import UploadFile
+
+class DataController(BaseController):
+    
+    def __init__(self):
+        super().__init__()
+        self.size_conversion = 1048576 # 1 MB = 1024 * 1024 bytes
+
+    # Validate the uploaded file type and size
+    # Returns True if valid, False otherwise
+    def validate_uploaded_file(self, file: UploadFile):
+        
+        if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES:
+            return False
+        
+        if file.size > self.app_settings.FILE_MAX_SIZE * self.size_conversion:
+            return False 
+        
+        return True
