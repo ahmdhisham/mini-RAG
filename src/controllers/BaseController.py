9a3add5b0afe5154cce63272e0a253052c122fd0
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-07-11 16:48:33 +0300

    Add self.files_dir to get the PATH of project_id

--- src/controllers/BaseController.py
+++ src/controllers/BaseController.py
@@ -1,6 +1,12 @@
-from src.helpers import get_settings, Settings 
+from src.helpers import get_settings, Settings
+import os 
 
 class BaseController:
     def __init__(self):
         
         self.app_settings = get_settings()
+        self.base_dir = os.path.dirname( os.path.dirname(__file__) )
+        self.files_dir = os.path.join(
+            self.base_dir,
+            "assets/files"
+        )
