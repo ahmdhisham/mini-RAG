02cb111f8970bb92cf4292148807dc72efd0a171
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-07-11 10:23:55 +0300

    Adjust imports to inherit path from the __init__ file directly

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 import os
-from src.helpers.config import get_settings, Settings
-from src.controllers.DataControllers import DataController
+from src.helpers import get_settings, Settings
+from src.controllers import DataController
 
 data_router = APIRouter(
     prefix = "/api/v1/data",
@@ -12,6 +12,7 @@ data_router = APIRouter(
 @data_router.post("/upload/{project_id}")
 async def upload_file(project_id: str, file: UploadFile, 
                     app_settings: Settings = Depends(get_settings)):
+    
     # validate the file type and size
     is_valid, result_signal = DataController().validate_uploaded_file(file = file)
     
