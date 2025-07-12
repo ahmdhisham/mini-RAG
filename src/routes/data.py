af3908ac9cb1aaeaaa011667fb8cc80145f67591
Ahmed Hisham <141911665+ahmdhisham@users.noreply.github.com>
2025-07-12 09:53:56 +0300

    Saving uploaded file to our src/assets/files/"project-id" directory

--- src/routes/data.py
+++ src/routes/data.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 import os
 from src.helpers import get_settings, Settings
-from src.controllers import DataController
+from src.controllers import DataController, ProjectController
+from src.models import ResponseSignals
+import aiofiles
 
 data_router = APIRouter(
     prefix = "/api/v1/data",
@@ -11,7 +13,7 @@ data_router = APIRouter(
 
 @data_router.post("/upload/{project_id}")
 async def upload_file(project_id: str, file: UploadFile, 
-                    app_settings: Settings = Depends(get_settings)):
+                      app_settings: Settings = Depends(get_settings)):
     
     # validate the file type and size
     is_valid, result_signal = DataController().validate_uploaded_file(file = file)
@@ -20,6 +22,22 @@ async def upload_file(project_id: str, file: UploadFile,
         return JSONResponse(
             status_code = status.HTTP_400_BAD_REQUEST,
             content = {
-                "Signal":result_signal
+                "signal":result_signal
             }
         )
+    
+    project_dir_path = ProjectController().get_project_path( project_id = project_id )
+    file_path = os.path.join(
+        project_dir_path,
+        file.filename
+    )
+    
+    async with aiofiles.open(file_path, "wb") as f:
+        while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+            await f.write(chunk)
+    
+    return JSONResponse(
+        content = {
+            "signal":ResponseSignals.FILE_UPLOAD_SUCCESS.value
+            }
+    )
